1f985672f3a00c26a8ec1b959d8f31916a42172c
Mark Yeh <yelexin@hotmail.com>
2018-11-03 13:39:11 +0800

    feat: add oschina, stackoverflow oauth

--- index.js
+++ index.js
@@ -22,20 +22,20 @@ const OAuthFactory = function OAuthFactory(provider) {
         case 'github':
             return function github(app, path, config, hook, toFrontEnd) {
                 // @TODO check config *******************
-                if(!config.clientId) {
+                if (!config.clientId) {
                     throw new Error('no clientId')
                 }
-                if(!config.clientId) {
+                if (!config.clientId) {
                     throw new Error('no clientSecret')
                 }
-                if(!config.state) {
+                if (!config.state) {
                     throw new Error('no state')
                 }
                 // *******************
                 return new Promise((resolve, reject) => {
                     try {
                         var option = url.parse(config.redirectURL)
-                        
+
                         // <a> link that user click
                         app.get(path, function (req, res, next) {
                             var arg = querystring.stringify({
@@ -62,18 +62,18 @@ const OAuthFactory = function OAuthFactory(provider) {
                                 console.log(data.data)
                                 var accessToken = data.data.access_token
                                 // load user info from github on behalf of the user
-                                return axios.get('https://api.github.com/user', {headers: {Authorization: 'token ' + accessToken}})
-                            
+                                return axios.get('https://api.github.com/user', { headers: { Authorization: 'token ' + accessToken } })
+
                             }).then(data => {
                                 hook(data.data)
                             })
-                            .then(()=>{
-                                res.redirect(toFrontEnd)
-                            })
-                            .catch(err => {
-                                console.log(err)
-                            })
-                            
+                                .then(() => {
+                                    res.redirect(toFrontEnd)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+
                         })
                         resolve(0)
                     } catch (err) {
@@ -84,11 +84,147 @@ const OAuthFactory = function OAuthFactory(provider) {
             }
             break
         case 'oschina':
-            return function oschina(config) {
+            return function oschina(app, path, config, hook, toFrontEnd) {
+                // @TODO check config *******************
+                if (!config.clientId) {
+                    throw new Error('no clientId')
+                }
+                if (!config.clientId) {
+                    throw new Error('no clientSecret')
+                }
+                if (!config.state) {
+                    throw new Error('no state')
+                }
+                // *******************
                 return new Promise((resolve, reject) => {
+                    try {
+                        var option = url.parse(config.redirectURL)
 
+                        // <a> link that user click
+                        app.get(path, function (req, res, next) {
+                            var arg = querystring.stringify({
+                                client_id: config.clientId,
+                                redirect_uri: config.redirectURL,
+                                response_type: config.responseType ? config.responseType : 'code',
+                                state: config.state,
+                            })
+                            res.redirect('https://www.oschina.net/action/oauth2/authorize?' + arg)
+                        })
+
+                        // redirect to which link from provider
+                        app.get(option.path, function (req, res, next) {
+                            var code = req.query.code
+                            // var refresh_token = 
+                            console.log('code', code)
+                            axios.get('https://www.oschina.net/action/openapi/token', {
+                                client_id: config.clientId,
+                                client_secret: config.clientSecret,
+                                grant_type: config.grantType ? config.grantType : 'authorization_code',
+                                redirect_uri: config.redirectURL,
+                                // refresh_token,
+                                state: config.state,
+                                dataType: config.dataType ? config.dataType : 'json',
+                                code,
+                                callback: config.callback ? config.callback : ''
+                            }).then(data => {
+                                console.log('token', data.data)
+                                var accessToken = data.data.access_token
+                                // load user info from github on behalf of the user
+                                return axios.get('https://www.oschina.net/action/openapi/user', {
+                                    access_token: accessToken,
+                                    dataType: config.dataType ? config.dataType : 'json',
+                                })
+
+                            }).then(data => {
+                                hook(data.data)
+                            }).then(() => {
+                                res.redirect(toFrontEnd)
+                            })
+                                .catch(err => {
+                                    console.log('token err ', err)
+                                })
+
+                        })
+                        resolve(0)
+                    } catch (err) {
+                        // @TODO
+                        reject(err)
+                    }
                 })
             }
+            break;
+        case 'stackoverflow':
+            return function github(app, path, config, hook, toFrontEnd) {
+                // @TODO check config *******************
+                if (!config.clientId) {
+                    throw new Error('no clientId')
+                }
+                if (!config.clientId) {
+                    throw new Error('no clientSecret')
+                }
+                if (!config.state) {
+                    throw new Error('no state')
+                }
+                if (!config.key) {
+                    throw new Error('no key')
+                }
+                // *******************
+                return new Promise((resolve, reject) => {
+                    try {
+                        var option = url.parse(config.redirectURL)
+
+                        // <a> link that user click
+                        app.get(path, function (req, res, next) {
+                            var arg = querystring.stringify({
+                                client_id: config.clientId,
+                                redirect_uri: config.redirectURL,
+                                scope: config.scope,
+                                state: config.state,
+                            })
+                            res.redirect('https://stackoverflow.com/oauth?' + arg)
+                        })
+
+                        // redirect to which link from provider
+                        app.get(option.path, function (req, res, next) {
+                            var code = req.query.code
+                            console.log('code', code)
+                            axios.post('https://stackoverflow.com/oauth/access_token/json', {
+                                client_id: config.clientId,
+                                client_secret: config.clientSecret,
+                                redirect_uri: config.redirectURL,
+                                // state: config.state,
+                                code,
+                            }, { headers: { Accept: 'application/json' } }).then(data => {
+                                console.log(data.data)
+                                var accessToken = data.data.access_token
+                                // load user info from github on behalf of the user
+                                return axios.get('https://api.stackexchange.com/2.2/me', {
+                                    params: {
+                                        access_token: accessToken,
+                                        key: config.key,
+                                        site: 'stackoverflow'
+                                    }
+                                })
+
+                            }).then(data => {
+                                hook(data.data.items[0])
+                            })
+                                .then(() => {
+                                    res.redirect(toFrontEnd)
+                                })
+                                .catch(err => {
+                                    console.log(err)
+                                })
+
+                        })
+                        resolve(0)
+                    } catch (err) {
+                        // @TODO
+                        reject(err)
+                    }
+                })
+            }
+            break;
     }
 
 }
--- test.js
+++ test.js
@@ -1,16 +1,35 @@
 const express = require('express')
 var app = express()
 const OAuthFactory = require('./index.js')
-var auth = OAuthFactory('github')
-auth(app, '/auth/github', {
-    clientId: 'c33213ad14962afa5840',
-    clientSecret: '6c6c41465595b9db786984792b21c1b2caac288f',
-    redirectURL: 'http://localhost:3000/code',
+// var auth = OAuthFactory('github')
+var oschinaAuth = OAuthFactory('oschina')
+var stackoverflow = OAuthFactory('stackoverflow')
+stackoverflow(app, '/auth/stackoverflow', {
+    clientId: '13491',
+    clientSecret: ')eUGlJ9zTtY1BmsGJAsKIQ((',
+    redirectURL: 'http://localhost:3000/handleOAuth/stackoverflow',
+    key: 'Bc9CleGdu2iTb67dr2risQ((',
     state: Math.random().toString(26).slice(2),
     scope: '',
-    allowSignup: true
+}, function (userData){
+    console.log('hook ', JSON.stringify(userData))
+}, 'http://localhost:8080/stackoverflowok')
+// auth(app, '/auth/github', {
+//     clientId: 'c33213ad14962afa5840',
+//     clientSecret: '6c6c41465595b9db786984792b21c1b2caac288f',
+//     redirectURL: 'http://localhost:3000/code',
+//     state: Math.random().toString(26).slice(2),
+//     scope: '',
+//     allowSignup: true
+// }, function (userData){
+//     console.log('hook ', userData)
+// }, 'http://localhost:8080/ojbk')
+oschinaAuth(app, '/auth/oschina', {
+    clientId: '3Xsh3OyJ978fsPHs5oAs',
+    clientSecret: 'Xi8XMFjqwlIg1R4bDzyrOvl9OlWvnvm7',
+    redirectURL: 'http://localhost:3000/oauth/oschina',
+    state: Math.random().toString(26).slice(2),
 }, function (userData){
     console.log('hook ', userData)
 }, 'http://localhost:8080/ojbk')
-
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
